f79cced2c709d6b1be7a0f7c1d21d8e26ce50634
rosarie <rosariosanchezsampietro@gmail.com>
2023-02-05 11:09:07 -0300

    Describe in setup

---
+++ demos/src/05-setup.test.js
@@ -0,0 +1,14 @@
+describe('set', () => {
+  test('case 1', () => {
+    console.log('case 1');
+    expect(1 + 1).toBe(2);
+  });
+  test('case 2', () => {
+    console.log('case 2');
+    expect(1 + 2).toBe(3);
+  });
+  test(('case 3'), () => {
+    console.log('case 3');
+    expect(4 + 5).toBe(9);
+  });
+});
